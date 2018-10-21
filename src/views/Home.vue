1b285a81a719244bff10327c9febf3f03cc9210f
Alex Napitupulu <sleigner@Alexs-MacBook-Pro.local>
2018-10-21 19:23:38 -0400

    Updates text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,13 +18,13 @@
     </v-navigation-drawer>
     <v-toolbar color="indigo" dark fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Kirim Uang</v-toolbar-title>
     </v-toolbar>
     <v-content>
       <router-view></router-view>
     </v-content>
     <v-footer color="indigo" app inset>
-      <span class="white--text">&copy; 2017</span>
+      <span class="white--text">&copy; 2018</span>
     </v-footer>
   </v-app>
 </template>
